b4e14f418ce45d4bde601634fd16b541066e5c35
viniciusaugusto84 <viniciusaugusto8464@gmail.com>
2026-06-21 00:11:16 -0300

    Initialize React Navigation with Login and Home screens

---
+++ Appp.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './screens/Login';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
